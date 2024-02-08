100aa158f92e2522ce84e7759a02e6432a8d37e0
adanglingpointer <dave@imail.cloud>
2024-02-07 20:55:37 -0500

    first commit

---
+++ src/main/cloud/imail/game/GuessingGame.java
@@ -0,0 +1,11 @@
+package cloud.imail.game;
+
+public class GuessingGame {
+    public String guess(int guessedNumber) {
+        return guessedNumber == getRandomNumber() ? "You got it" : "You didn't get it";
+    }
+
+    public int getRandomNumber() {
+        return 0;
+    }
+}
---
+++ src/test/cloud/imail/game/GuessingGameTest.java
@@ -0,0 +1,31 @@
+package cloud.imail.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessingGameTest {
+
+    @Test
+    public void testSimpleWinSituation() {
+        GuessingGame game = new GuessingGame();
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum);
+        assertEquals("You got it", message);
+    }
+
+    @Test
+    public void testOneWrongNegGuessSituation() {
+        GuessingGame game = new GuessingGame();
+        String message = game.guess(-5);
+        assertEquals("You didn't get it", message);
+    }
+
+    @Test
+    public void testOneWrongPosGuessSituation() {
+        GuessingGame game = new GuessingGame();
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum + 1);
+        assertEquals("You didn't get it", message);
+    }
+}
