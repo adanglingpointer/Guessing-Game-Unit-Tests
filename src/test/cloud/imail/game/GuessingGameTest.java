f1594bf09dac694e68b828ecebc3d3d938c9daa7
adanglingpointer <dave@imail.cloud>
2024-02-08 20:42:16 -0500

    testing random number generation

--- src/test/cloud/imail/game/GuessingGameTest.java
+++ src/test/cloud/imail/game/GuessingGameTest.java
@@ -1,14 +1,21 @@
 package cloud.imail.game;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
 
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GuessingGame();
+    }
+
     @Test
     public void testSimpleWinSituation() {
-        GuessingGame game = new GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum);
         assertEquals("You got it", message);
@@ -16,16 +23,29 @@ public class GuessingGameTest {
 
     @Test
     public void testOneWrongNegGuessSituation() {
-        GuessingGame game = new GuessingGame();
         String message = game.guess(-5);
         assertEquals("You didn't get it", message);
     }
 
     @Test
     public void testOneWrongPosGuessSituation() {
-        GuessingGame game = new GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum + 1);
         assertEquals("You didn't get it", message);
+    }
+
+    @Test
+    public void testRandomNumberGeneration() {
+        int[] rndNumCount = new int[11];
+        for (int counter=0; counter < 100; counter++) {
+            int randomNum = game.getRandomNumber();
+            rndNumCount[randomNum] = 1;
+        }
+
+        int sum = 0;
+        for (int counter=0; counter < 11; counter++) {
+            sum+=rndNumCount[counter];
+        }
+        assertEquals(10, sum);
     }
 }
